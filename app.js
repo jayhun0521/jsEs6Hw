95676e3b0cbca524a66128731bcd485b528b8f91
jayhun0521 <jayhun21@gmail.com>
2022-10-16 18:15:17 -0400

    new push

---
+++ app.js
@@ -0,0 +1,51 @@
+var a = 4;
+var b = 6;
+
+function mySum(a,b){
+    return a + b;
+}
+
+console.log(mySum (a,b));
+
+var mySum = (a, b) => {
+    return a + b;
+}
+
+console.log (mySum(a,b));
+
+
+
+function mySubtract(a, b){
+    return a - b;
+}
+
+var mySubtract = (a, b) => {
+    return a - b;
+}
+
+console.log (mySubtract (a, b));
+
+
+function myMultiply(a,b){
+    return a * b;
+}
+
+var myMultiply = (a, b) => {
+    return a * b;
+}
+
+console.log (myMultiply (a, b));
+
+
+
+function myDivide(a,b){
+    return a / b;
+}
+
+var myDivide = (a, b) => {
+    return a / b;
+}
+
+console.log(myDivide (a, b));
+
+
